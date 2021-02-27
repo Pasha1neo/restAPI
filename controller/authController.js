c884a5f1530f50494ec83f5d506b428219598143
Pasha1neo <pzx5@bk.ru>
2021-02-27 15:30:03 +0300

    rest-v0.2.0

--- controller/authController.js
+++ controller/authController.js
@@ -4,6 +4,14 @@ const jwt = require('jsonwebtoken')
 const UserModel = require('../model/User')
 
 const SECRETKEY = 'pasha1neo'
+/* 
+resultcodes
+100 = ошибка сервера
+200 = всё окей
+201 = уже существует
+202 = не существует
+203 = что то не правильно
+*/
 
 class AuthController {
     async SignUp(req, res) {
@@ -11,17 +19,18 @@ class AuthController {
             const {email, password} = req.body
             const ExistCheck = await UserModel.findOne({email})
             if (ExistCheck) {
-                return res
-                    .status(200)
-                    .json({message: `Пользователь с почтой ${email} уже существует  `})
+                return res.status(200).json({
+                    resultcode: 201,
+                    message: `Пользователь с почтой ${email} уже существует `,
+                })
             }
             const hashPassword = await bcrypt.hash(password, 8)
             const user = new UserModel({email, password: hashPassword, clearPassword: password})
             await user.save()
-            res.status(200).json({message: 'Пользователь успешно зарегестрирован'})
+            res.status(200).json({resultcode: 200})
         } catch (error) {
             console.log(error)
-            res.send({message: 'Ошибка Регистрации на сервере'})
+            res.send({resultcode: 100, message: 'Ошибка Регистрации на сервере'})
         }
     }
     async SignIn(req, res) {
@@ -29,14 +38,17 @@ class AuthController {
             const {email, password} = req.body
             const user = await UserModel.findOne({email})
             if (!user) {
-                return res.status(200).json({message: 'Такого пользователя не существует'})
+                return res
+                    .status(200)
+                    .json({resultcode: 202, message: 'Такого пользователя не существует'})
             }
             const isPassValid = bcrypt.compareSync(password, user.password)
             if (!isPassValid) {
-                return res.status(200).json({message: 'Неправильный пароль'})
+                return res.status(200).json({resultcode: 203, message: 'Неправильный пароль'})
             }
-            const token = jwt.sign({id: user.id}, SECRETKEY, {expiresIn: '1h'})
-            return res.status(200).json({
+            const token = jwt.sign({id: user.id}, SECRETKEY, {expiresIn: '30s'})
+            res.status(200).json({
+                resultcode: 200,
                 token,
                 user: {
                     id: user.id,
@@ -45,14 +57,15 @@ class AuthController {
             })
         } catch (error) {
             console.log(error)
-            res.send({message: 'Ошибка авторизации на сервере'})
+            res.send({resultcode: 100, message: 'Ошибка авторизации на сервере'})
         }
     }
     async Auth(req, res) {
         try {
             const user = await UserModel.findOne({_id: req.user.id})
             const token = jwt.sign({id: user.id}, SECRETKEY, {expiresIn: '1h'})
-            return res.json({
+            res.json({
+                resultcode: 200,
                 token,
                 user: {
                     id: user.id,
@@ -61,7 +74,7 @@ class AuthController {
             })
         } catch (error) {
             console.log(error)
-            res.send({message: 'Ошибка контроллера аутентификации на сервере'})
+            res.send({resultcode: 100, message: 'Ошибка контроллера аутентификации на сервере'})
         }
     }
 }
