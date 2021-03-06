9896c2e9582053e556b47ddf2d654d6dc808c911
Pasha1neo <pzx5@bk.ru>
2021-03-06 19:52:14 +0300

    predeploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const authRouter = require('./route/auth')
-
 const socketController = require('./socket-module')
 const io = require('socket.io')(server)
 app.use(express.json())
@@ -14,10 +13,13 @@ app.use('/api/auth', authRouter)
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/project-adaptive', {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
+        await mongoose.connect(
+            'mongodb+srv://Pasha1neo:pasha1neo@cluster0.lwubz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+            }
+        )
         io.on('connection', (socket) => {
             socketController(socket, io)
         })
@@ -25,7 +27,6 @@ const start = async () => {
             if (err) {
                 throw Error(err)
             }
-            console.log(`Сервер запущен на  http://localhost:${PORT}/`)
         })
     } catch (error) {
         console.log(error)
