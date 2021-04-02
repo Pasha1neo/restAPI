28c6ccb4fe7c3b8073655ffcf2d6cc9d0be1f11b
Pasha1neo <pzx5@bk.ru>
2021-04-02 18:34:08 +0300

    fix db

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/api/auth', authRouter)
 const start = async () => {
     try {
         await mongoose.connect(
-            'mongodb+srv://Pasha1neo:<password>@cluster0.lwubz.mongodb.net/projectAdaptive',
+            'mongodb+srv://Pasha1neo:<password>@cluster0.lwubz.mongodb.net/users',
             {
                 useUnifiedTopology: true,
                 useNewUrlParser: true,
