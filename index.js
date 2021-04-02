61f6456151be2795d05d9b3ccc3f7ad0aca15856
Pasha1neo <pzx5@bk.ru>
2021-04-02 17:51:33 +0300

    deploy 02.04.2021 17:51

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express()
 const authRouter = require('./route/auth')
 const {socketModule} = require('./socket-module')
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'https://project-adaptive.herokuapp.com/',
+        methods: ['GET', 'POST'],
+    },
+})
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth', authRouter)
@@ -14,10 +19,13 @@ app.use('/api/auth', authRouter)
 //воспользоваться штуками из книги (телефон)
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/project-adaptive', {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
+        await mongoose.connect(
+            'mongodb+srv://Pasha1neo:pasha1neo@cluster0.lwubz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+            }
+        )
         socketModule(io)
     } catch (error) {
         console.log(error)
